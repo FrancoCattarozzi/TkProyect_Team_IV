4f894cb7409912011a25b08bb613eaeddc071d5d
Sergio.R1 Talavera <talaveras685@gmail.com>
2025-06-10 21:00:03 -0300

    centro la ventana

--- barra_desplazamiento.py
+++ barra_desplazamiento.py
@@ -22,4 +22,14 @@ tk.BOTH)
 
 scrollbar .config(command = lista.yview)
 
+# Centrar la ventana en la pantalla
+ventana.update_idletasks()
+ancho_ventana = 400
+alto_ventana = 200
+ancho_pantalla = ventana.winfo_screenwidth()
+alto_pantalla = ventana.winfo_screenheight()
+x = (ancho_pantalla // 2) - (ancho_ventana // 2)
+y = (alto_pantalla // 2) - (alto_ventana // 2)
+ventana.geometry(f"{ancho_ventana}x{alto_ventana}+{x}+{y}")
+
 ventana.mainloop ()
